591cc97aa05b5ab870df8a4be62777643fc6fbf4
moquan4543 <moquan4543@gmail.com>
2023-12-25 21:53:10 +0800

    fix output

--- src/command/Return.java
+++ src/command/Return.java
@@ -12,7 +12,7 @@ public class Return implements Command{
     @Override
     public void execute(User invoker, String arg) throws RuntimeException {
         if(!invoker.getUserType().equals(priority.Staff)){
-            throw new PermissionDeniedException("Borrower can not add book");
+            throw new PermissionDeniedException("Borrower can not return book");
         }
         Library lib = Library.getInstance();
         Integer returnedBookID = Integer.parseInt(arg);
