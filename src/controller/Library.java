a999111a63b4abd9902c5a0cbce8de6a0596880e
moquan4543 <moquan4543@gmail.com>
2023-12-30 19:28:34 +0800

    YinChuan Update

--- src/controller/Library.java
+++ src/controller/Library.java
@@ -4,6 +4,7 @@ import object.Book;
 import object.User;
 import java.io.BufferedReader;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Library {
     public Map<String, User> users = new HashMap<>();
@@ -32,7 +33,7 @@ public class Library {
         //輸入的單行指令由空格隔開(可以是多個)
         //透過stream的方法轉成List，構造成LinkedList
         //透過LinkedList提供的方法模擬隊列，避免索引寫死如果沒有操作數會越界的問題
-        LinkedList<String> strQueue = new LinkedList<>(Arrays.stream(cmdStr.split("\\s+")).toList());
+        LinkedList<String> strQueue = new LinkedList<>(Arrays.stream(cmdStr.split("\\s+")).collect(Collectors.toList()));
         User invoker = users.get(strQueue.removeFirst());
         String instruction = strQueue.removeFirst();
         String arg;
