95954c0f84e8c25574f1d31f8a44f27e5bb73bdf
moquan4543 <moquan4543@gmail.com>
2023-12-30 19:29:04 +0800

    book not exist handling

--- src/command/Return.java
+++ src/command/Return.java
@@ -12,10 +12,14 @@ public class Return implements Command{
     @Override
     public void execute(User invoker, String arg) throws RuntimeException {
         if(!invoker.getUserType().equals(priority.Staff)){
-            throw new PermissionDeniedException("Borrower can not return book");
+            throw new PermissionDeniedException("Borrower can not return book.");
         }
         Library lib = Library.getInstance();
         Integer returnedBookID = Integer.parseInt(arg);
-        lib.books.get(returnedBookID).doReturn();
+        try{
+            lib.books.get(returnedBookID).doReturn();
+        }catch(NullPointerException e){
+            throw new NullPointerException("Can not return book since the book does not exist.");
+        }
     }
 }
